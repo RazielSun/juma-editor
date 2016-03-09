976fec2b8677aeffc00f49d66b30f638d58a47c3
Aleksandr Kardakov <razielsun@gmail.com>
2016-03-09 10:31:51 +0300

    first init project

---
+++ editor/lib/juma/core/Project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import logging
+import sys
+import imp
+import os
+import os.path
+import re
+import shutil
+import hashlib
+import time
+
+import signals
+import jsonHelper
+
+##----------------------------------------------------------------##
+class Project(object):
+
+	def __init__(self):
+		pass
